b043638adeb0f757c1c9492d4884b8a6b18c7ed7
lesley <lesleybanadzem2017@gmail.com>
2020-02-19 09:10:49 -0800

    Added a dummy link array and root a root query for querying all the links in the link array and a schema def of how the Link object is

--- server/src/index.js
+++ server/src/index.js
@@ -1,21 +1,43 @@
 const { GraphQLServer } = require('graphql-yoga');
 
+// Schema Defination
 const typeDefs = `
     type Query {
         info: String!
+        feed: [Link!]!
+    }
+    
+    type Link {
+        id: ID!,
+        description: String!
+        url: String!
     }
 `;
 
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+  }]
+  
 
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews clone`,
-    }
+        info: () =>`This is the API of a Hackernews clone`,
+        feed: () => links
+    },
+
+    Link: (parent, args, content) => ({
+        id: parent.id,
+        description: parent.description,
+        url: parent
+    })
+    
 };
 
 const server = new GraphQLServer({
     typeDefs,
-    resolvers
+    resolvers,
 });
 
 server.start(()=> console.log(`Server is running on http://localhost:4000`))
