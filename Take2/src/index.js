b254dce3afe99e1ecaf0373ef1a9c14c3c5e8718
lesley <lesleybanadzem2017@gmail.com>
2020-04-23 12:08:35 -0700

    Added a feed query to return all links and a feeds resolver to resolve the query

--- Take2/src/index.js
+++ Take2/src/index.js
@@ -1,6 +1,5 @@
 const { GraphQLServer } = require("graphql-yoga");
 
-
 /**
  * Type Definition for describing our schema. Also note that the exclamation sign after the string means it can't be null
 
@@ -8,26 +7,43 @@ const { GraphQLServer } = require("graphql-yoga");
 const typeDefs = `
     type Query{
         info: String!
+        feed: [Link!]!
+        link(): Link
     }
-
+    type Link{
+        id: ID!
+        url: String!
+        description: String!
+    }
 `;
 
+let links = [
+  { id: 1, url: "f.com", description: "some" },
+  { id: 2, url: "f.com", description: "some" },
+  { id: 3, url: "f.com", description: "some" },
+];
 /**
  * This is the implementation of the schema definition. Resolvers are where we implement our type definitions
  */
 const resolvers = {
-    Query: {
-        info: () => "Hello, Welcome to graphql",
-    }
-}
+  Query: {
+    info: () => "Hello, Welcome to graphql",
+    feed: () => links,
+  },
 
+  Link: {
+      id: parent => parent.id,
+      url: parent => parent.url,
+      description: parent => parent.description
+  }
+};
 
 /**
- * Finally we pass the typedef and the resolve into a new instance of Graphqlserver to start our server which then execute the resolver 
+ * Finally we pass the typedef and the resolve into a new instance of Graphqlserver to start our server which then execute the resolver
  */
 const server = new GraphQLServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
-server.start(()=> console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log(`Server is running on http://localhost:4000`));
