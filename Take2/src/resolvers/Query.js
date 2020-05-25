93f10c67671b628afb5b88d329eee5ebbc257bad
lesley <lesleybanadzem2017@gmail.com>
2020-05-24 17:55:23 -0700

    Added Filter to the feed query

--- Take2/src/resolvers/Query.js
+++ Take2/src/resolvers/Query.js
@@ -1,13 +1,25 @@
 class Query {
+  async feed(parent, args, context) {
+    //If no filter is provided we send an empty object which is just going to return all the strings
+    //If filter is provided we construct an object that prisma uses to filter through the database and return the links that match the filter
+    const where = args.filter
+      ? {
+          OR: [
+            { description_contains: args.filter },
+            { url_contains: args.filter },
+          ],
+        }
+      : {};
 
-    feed(parent, args, context){
-        
-        return context.prisma.links();
-    }
-    
-    info(){
-        return "Hello Prisma World"
-    }
+    const links = await context.prisma.links({
+      where,
+    });
+    return links;
+  }
+
+  info() {
+    return "Hello Prisma World";
+  }
 }
 
 module.exports = new Query();
