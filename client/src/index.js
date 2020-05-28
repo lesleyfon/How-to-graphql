7aa3657e0e6b17c06e41f3f453d8d2b6db09e49b
lesley <lesleybanadzem2017@gmail.com>
2020-05-28 10:48:54 -0700

    created a file structue for the app and moved files respectively

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
+
+//Third party Imports
+import { ApolloProvider } from "react-apollo";
+import { ApolloClient } from "apollo-client";
+import { createHttpLink } from "apollo-link-http";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+//Components
+import App from "./components/App";
+
+//Styles
+import "././styles/index.css";
+
+const httpLink = createHttpLink({
+  uri: "http://localhost:4000",
+});
+
+const client = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+});
+
+console.log(client);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById("root")
 );
