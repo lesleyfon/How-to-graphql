b3c044e58863bcb5cc16c90176d27971e05fa659
lesley <lesleybanadzem2017@gmail.com>
2020-05-28 15:04:35 -0700

    Configured apollo-client to set the authorization header when a user tries to create a mutation or a query using apollo-link-context

--- client/src/index.js
+++ client/src/index.js
@@ -7,19 +7,31 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
-
+import { setContext } from "apollo-link-context";
 //Components
 import App from "./components/App";
 
 //Styles
 import "././styles/index.css";
+import { AUTH_TOKEN } from "./constant";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      Authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
+
   cache: new InMemoryCache(),
 });
 
