3245a29dfa87496767e05354ad3583b3c67c5ca6
lesley <lesleybanadzem2017@gmail.com>
2020-12-26 16:06:38 -0600

    Update

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,58 +5,56 @@ import { withRouter } from "react-router-dom";
 import { AUTH_TOKEN } from "../constant";
 
 class Header extends Component {
-  componentDidMount() {
-    const authToken = localStorage.getItem(AUTH_TOKEN);
-    authToken
-      ? this.props.history.push("/")
-      : this.props.history.push("/login");
-  }
-  render() {
-    const authToken = localStorage.getItem(AUTH_TOKEN);
+	componentDidMount() {
+		const authToken = localStorage.getItem(AUTH_TOKEN);
+		authToken ? this.props.history.push("/") : this.props.history.push("/login");
+	}
+	render() {
+		const authToken = localStorage.getItem(AUTH_TOKEN);
 
-    return (
-      <div className="flex pa1 justify-between nowrap orange">
-        <div className="flex flex-fixed black">
-          <div className="fw7 mr1">Hacker News</div>
-          <Link to="/" className="ml1 no-underline black">
-            new
-          </Link>
+		return (
+			<div className="flex pa1 justify-between nowrap orange">
+				<div className="flex flex-fixed black">
+					<div className="fw7 mr1">Hacker News</div>
+					<Link to="/" className="ml1 no-underline black">
+						Home
+					</Link>
 
-          {authToken && (
-            <>
-              <div className="ml1">|</div>
-              <Link to="/create" className="ml1 no-underline black">
-                submit
-              </Link>
-            </>
-          )}
+					{authToken && (
+						<>
+							<div className="ml1">|</div>
+							<Link to="/create" className="ml1 no-underline black">
+								submit
+							</Link>
+						</>
+					)}
 
-          {authToken ? (
-            <>
-              <div className="ml1">|</div>
-              <Link
-                to="/login"
-                className="ml1 no-underline black"
-                onClick={() => {
-                  localStorage.removeItem(AUTH_TOKEN);
-                  this.props.history.push("/login");
-                }}
-              >
-                Logout
-              </Link>
-            </>
-          ) : (
-            <>
-              <div className="ml1">|</div>
-              <Link to="/login" className="ml1 no-underline black">
-                Login
-              </Link>
-            </>
-          )}
-        </div>
-      </div>
-    );
-  }
+					{authToken ? (
+						<>
+							<div className="ml1">|</div>
+							<Link
+								to="/login"
+								className="ml1 no-underline black"
+								onClick={() => {
+									localStorage.removeItem(AUTH_TOKEN);
+									this.props.history.push("/login");
+								}}
+							>
+								Logout
+							</Link>
+						</>
+					) : (
+						<>
+							<div className="ml1">|</div>
+							<Link to="/login" className="ml1 no-underline black">
+								Login
+							</Link>
+						</>
+					)}
+				</div>
+			</div>
+		);
+	}
 }
 
 export default withRouter(Header);
--- client/src/components/LogIn.js
+++ client/src/components/LogIn.js
@@ -4,131 +4,126 @@ import { Mutation } from "react-apollo";
 import { AUTH_TOKEN } from "./../constant";
 
 export default class LogIn extends Component {
-  state = {
-    login: true,
-    email: "",
-    name: "",
-    password: "",
-    error: {
-      status: false,
-      message: "",
-    },
-  };
-  render() {
-    const { email, password, login, name } = this.state;
-    const LOGIN_MUTATION = gql`
-      mutation LoginMutation($email: String!, $password: String!) {
-        login(email: $email, password: $password) {
-          token
-          user {
-            name
-            email
-            id
-          }
-        }
-      }
-    `;
-    const SINGUP_MUTATION = gql`
-      mutation SignMutation(
-        $name: String!
-        $email: String!
-        $number: Int
-        $password: String!
-      ) {
-        signUp(
-          name: $name
-          email: $email
-          number: $number
-          password: $password
-        ) {
-          token
-          user {
-            name
-            email
-            id
-          }
-        }
-      }
-    `;
-    return (
-      <div>
-        <h4 className="mv3">{login ? "Login" : "Sign Up"}</h4>
-        {this.state.error && (
-          <span className="error">{this.state.error.message}</span>
-        )}
-        <div className="flex flex-column">
-          {!login && (
-            <input
-              name="name"
-              value={name}
-              placeholder="name"
-              onChange={(event) => this.setState({ name: event.target.value })}
-            />
-          )}
-          <input
-            name="email"
-            value={email}
-            placeholder="Email"
-            onChange={(event) => this.setState({ email: event.target.value })}
-          />
-          <input
-            name="password"
-            type="password"
-            value={password}
-            placeholder="Password"
-            onChange={(event) =>
-              this.setState({ password: event.target.value })
-            }
-          />
-          <div className="flex mt3">
-            <Mutation
-              mutation={login ? LOGIN_MUTATION : SINGUP_MUTATION}
-              variables={{ name, email, password }}
-              onCompleted={(data) => {
-                this._confirm(data);
-              }}
-              onError={(error) => {
-                const message = error.message.split(":")[1];
+	state = {
+		login: true,
+		email: "",
+		name: "",
+		password: "",
+		error: {
+			status: false,
+			message: "",
+		},
+	};
+	render() {
+		const { email, password, login, name } = this.state;
+		const LOGIN_MUTATION = gql`
+			mutation LoginMutation($email: String!, $password: String!) {
+				login(email: $email, password: $password) {
+					token
+					user {
+						name
+						email
+						id
+					}
+				}
+			}
+		`;
+		const SINGUP_MUTATION = gql`
+			mutation SignMutation(
+				$name: String!
+				$email: String!
+				$number: Int
+				$password: String!
+			) {
+				signUp(name: $name, email: $email, number: $number, password: $password) {
+					token
+					user {
+						name
+						email
+						id
+					}
+				}
+			}
+		`;
+		return (
+			<div>
+				<h4 className="mv3">{login ? "Login" : "Sign Up"}</h4>
+				{this.state.error && <span className="error">{this.state.error.message}</span>}
+				<div className="flex flex-column">
+					{!login && (
+						<input
+							name="name"
+							value={name}
+							placeholder="name"
+							onChange={(event) => this.setState({ name: event.target.value })}
+						/>
+					)}
+					<input
+						name="email"
+						value={email}
+						placeholder="Email"
+						onChange={(event) => this.setState({ email: event.target.value })}
+					/>
+					<input
+						name="password"
+						type="password"
+						value={password}
+						placeholder="Password"
+						onChange={(event) => this.setState({ password: event.target.value })}
+					/>
+					<div className="flex mt3">
+						<Mutation
+							mutation={login ? LOGIN_MUTATION : SINGUP_MUTATION}
+							variables={{ name, email, password }}
+							onCompleted={(data) => {
+								this._confirm(data);
+							}}
+							onError={(error) => {
+								const message = error.message.split(":")[1];
 
-                this.setState({
-                  error: {
-                    status: !this.state.error.status,
-                    message: message,
-                  },
-                });
-              }}
-            >
-              {(mutation) => (
-                <div className="pointer mr2 button" onClick={mutation}>
-                  {login ? "login" : "create account"}
-                </div>
-              )}
-            </Mutation>
+								this.setState({
+									error: {
+										status: !this.state.error.status,
+										message: message,
+									},
+								});
+							}}
+						>
+							{(mutation) => (
+								<div className="pointer mr2 button" onClick={mutation}>
+									{login ? "login" : "create account"}
+								</div>
+							)}
+						</Mutation>
 
-            <div
-              className="pointer button"
-              onClick={() => this.setState({ login: !login })}
-            >
-              {" "}
-              {login
-                ? "need to create an account?"
-                : "already have an account?"}
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+						<div
+							className="pointer button"
+							onClick={() => this.setState({ login: !login })}
+						>
+							{" "}
+							{login ? "need to create an account?" : "already have an account?"}
+						</div>
+					</div>
+				</div>
+			</div>
+		);
+	}
 
-  _confirm = async (data) => {
-    const {
-      login: { token },
-    } = data;
-    this._saveUserData(token);
-    this.props.history.push("/");
-  };
+	_confirm = async (data) => {
+		let token;
+		if (data.hasOwnProperty("login")) {
+			token = data.login.token;
+		} else if (data.hasOwnProperty("signUp")) {
+			token = data.signUp.token;
+		} else if (data.hasOwnProperty("signup")) {
+			token = data.signup.token;
+		}
+		console.log(token);
+		this._saveUserData(token);
+		this.props.history.push("/");
+	};
 
-  _saveUserData = (token) => {
-    localStorage.setItem(AUTH_TOKEN, token);
-  };
+	_saveUserData = (token) => {
+		localStorage.setItem(AUTH_TOKEN, token);
+	};
 }
